a9efb4849edce09b356bb3fb9e8af470a51f8d86
David11253 <minerusiapiter58@gmail.com>
2025-01-25 15:28:13 +0600

    Add files via upload

---
+++ тортики/know.js
@@ -0,0 +1,22 @@
+export const torts = [
+    {
+        name: "Наполеон",
+        ingest: "Мука, масло, молоко, сахар, яйца",
+        youtube_srs: "https://www.youtube.com/embed/Видео1" // Замени на реальную ссылку
+    },
+    {
+        name: "Медовик",
+        ingest: "Мука, мед, масло, сахар, сметана",
+        youtube_srs: "https://www.youtube.com/embed/Видео2"
+    },
+    {
+        name: "Чизкейк",
+        ingest: "Сыр, сливки, печенье, сахар",
+        youtube_srs: "https://www.youtube.com/embed/Видео3"
+    },
+    {
+        name: "Шоколадный торт",
+        ingest: "Какао, мука, масло, сахар, яйца",
+        youtube_srs: "https://www.youtube.com/embed/Видео4"
+    }
+];
